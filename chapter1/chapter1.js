40ec6bee6b64927811ea4bf19903467c89b1e058
Sheldon John Van Zyl <123169408+SheldonVZyl@users.noreply.github.com>
2023-07-07 10:25:36 +0200

    Add files via upload

---
+++ chapter1/chapter1.js
@@ -0,0 +1,58 @@
+console.log("hello world!");
+
+
+//extension.js
+
+function addTwoNumbers(x = 2, y = 3) {
+    console.log(x + y);
+   }
+
+addTwoNumbers();
+addTwoNumbers(6, 6);
+addTwoNumbers(10);
+addTwoNumbers(1,2,3,4)
+
+/*PRACTICE EXERCISE 1.2*/
+
+/*let status = "new";
+let scared = true;
+if (status === "new") {
+console.log("Welcome to JavaScript!");
+if (scared) {
+console.log("Don't worry you will be fine!");
+} else {
+console.log("You're brave! You are going to do great!");
+}
+} else {
+console.log("Welcome back, I knew you'd like it!");
+}*/
+
+/*let status = "new";
+let scared = true;
+if (status === "new") {
+ console.log("Welcome to JavaScript!");
+ if (scared) {
+ console.log("Don't worry you will be fine!");
+ } else {
+ console.log("You're brave! You are going to do great!");
+ }
+} else {
+ console.log("Welcome back, I knew you'd like it!");
+}
+
+/*eXE1.4*/
+
+/*let a = 10;
+console.log(a);
+
+prompt("Hi! How are you?");
+
+console.log(Math.random() * 100);*/
+
+/*exercise1.1*/
+
+/*55+10
+console.log("Jeffrey");*/
+
+
+
